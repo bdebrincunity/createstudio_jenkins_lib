ac3542aca43450206024c63f61f0707f2d520a4d
Jas Rowinski <jas.rowinski@unity3d.com>
2020-07-20 12:14:19 -0700

    updating more variables

--- vars/multiStagePipeline.groovy
+++ vars/multiStagePipeline.groovy
@@ -18,7 +18,7 @@ def call(body) {
         // Some global default variables
         environment {
             IMAGE_NAME = "${pipelineParams.SERVICE_NAME}"
-            TEST_LOCAL_PORT = 8817
+            TEST_LOCAL_PORT = 8080
             DEPLOY_PROD = false
             DOCKER_TAG = 'dev'
             DOCKER_REG = 'gcr.io/unity-labs-createstudio-test'
@@ -103,7 +103,7 @@ def call(body) {
                             sh "[ -z \"\$(docker ps -a | grep ${ID} 2>/dev/null)\" ] || docker rm -f ${ID}"
         
                             echo "Starting ${IMAGE_NAME} container"
-                            sh "docker run --detach --name ${ID} --rm --publish ${TEST_LOCAL_PORT}:80 pipelineParams.DOCKER_REG/${IMAGE_NAME}:${DOCKER_TAG}"
+                            sh "docker run --detach --name ${ID} --rm --publish ${TEST_LOCAL_PORT}:80 ${DOCKER_REG}/${IMAGE_NAME}:${DOCKER_TAG}"
     
                             host_ip = sh(returnStdout: true, script: '/sbin/ip route | awk \'/default/ { print $3 ":${TEST_LOCAL_PORT}" }\'')
                         }
