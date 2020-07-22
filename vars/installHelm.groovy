40dcafe740872adc4912c39bb4001fa53e07d3a9
Jas Rowinski <jas.rowinski@unity3d.com>
2020-07-22 11:05:11 -0700

    install openssl

--- vars/installHelm.groovy
+++ vars/installHelm.groovy
@@ -4,7 +4,7 @@
 def call () {
     script {
         sh """
-            apk add curl bash
+            apk add curl bash openssl
             curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
             chmod 700 get_helm.sh
             ./get_helm.sh
