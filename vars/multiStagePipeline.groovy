7514fa80ed1f432d187c67a754db73c5eda1700c
Jas Rowinski <jas.rowinski@unity3d.com>
2020-07-22 12:46:12 -0700

    removing .inside for docker container

--- vars/multiStagePipeline.groovy
+++ vars/multiStagePipeline.groovy
@@ -175,7 +175,7 @@ def call(body) {
                             echo "Deploying application ${ID} to ${env} kubernetes cluster "
                             // createNamespace (namespace)
     
-                            docker.image("kiwigrid/gcloud-kubectl-helm").inside("-w /workspace -v \${PWD}:/workspace -it") {
+                            docker.image("kiwigrid/gcloud-kubectl-helm") {
                                 echo "Downloading k8s config"
                                 downloadFile("k8s/configs/${env}/kubeconfig-labs-createstudio-${env}_environment", 'createstudio_ci_cd')
 //                                installHelm()
