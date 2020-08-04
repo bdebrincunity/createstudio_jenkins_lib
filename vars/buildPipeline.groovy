1eff96e7b0712ca0ef971f0874928252f12dc23b
Jas Rowinski <jas.rowinski@unity3d.com>
2020-08-04 13:48:29 -0700

    lets see if we can switch dir with a container

--- vars/buildPipeline.groovy
+++ vars/buildPipeline.groovy
@@ -96,21 +96,23 @@ def call(body) {
             ////////// Step 2 //////////
             stage('Build and tests') {
                 steps {
-                    container('docker') {
-                        script {
-                            echo "Building application and Docker image"
-                            //myapp = BuildDockerImage(registry: registry, returnStdout: true)
-                            myapp = sh("docker build -t ${DOCKER_REG}/${IMAGE_NAME} . || errorExit \"Building ${IMAGE_NAME} failed\"")
+                    dir("${PROJECT_DIR}") {
+                        container('docker') {
+                            script {
+                                echo "Building application and Docker image"
+                                //myapp = BuildDockerImage(registry: registry, returnStdout: true)
+                                myapp = sh("docker build -t ${DOCKER_REG}/${IMAGE_NAME} . || errorExit \"Building ${IMAGE_NAME} failed\"")
 
-                            echo "Running local docker tests"
+                                echo "Running local docker tests"
 
-                            // Kill container in case there is a leftover
-                            sh "[ -z \"\$(docker ps -a | grep ${IMAGE_NAME} 2>/dev/null)\" ] || docker rm -f ${IMAGE_NAME}"
+                                // Kill container in case there is a leftover
+                                sh "[ -z \"\$(docker ps -a | grep ${IMAGE_NAME} 2>/dev/null)\" ] || docker rm -f ${IMAGE_NAME}"
 
-                            echo "Starting ${IMAGE_NAME} container"
-                            sh "docker run --detach --name ${IMAGE_NAME} --rm --publish ${TEST_LOCAL_PORT}:80 ${DOCKER_REG}/${IMAGE_NAME}"
+                                echo "Starting ${IMAGE_NAME} container"
+                                sh "docker run --detach --name ${IMAGE_NAME} --rm --publish ${TEST_LOCAL_PORT}:80 ${DOCKER_REG}/${IMAGE_NAME}"
 
-                            host_ip = sh(returnStdout: true, script: '/sbin/ip route | awk \'/default/ { print $3 ":${TEST_LOCAL_PORT}" }\'')
+                                host_ip = sh(returnStdout: true, script: '/sbin/ip route | awk \'/default/ { print $3 ":${TEST_LOCAL_PORT}" }\'')
+                            }
                         }
                     }
                 }
