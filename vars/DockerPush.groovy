38f85840801f4c73d11f25f1355727091260b2b2
Jas Rowinski <jas.rowinski@unity3d.com>
2020-09-16 10:12:17 -0700

    updating to use name_id and setting it to be lowercase for docker push

--- vars/DockerPush.groovy
+++ vars/DockerPush.groovy
@@ -14,7 +14,7 @@ def call(Map args = [:]) {
 
     withEnv([
             "version=${sh([returnStdout: true, script: 'echo $CURRENT_VERSION']).trim()}",
-            "service_name=${sh([returnStdout: true, script: 'echo $SERVICE_NAME']).trim()}",
+            "name_id=${sh([returnStdout: true, script: 'echo $NAME_ID']).toLowerCase}",
             "registry=${sh([returnStdout: true, script: 'echo $DOCKER_REG']).trim()}",
             "revision=${sh([returnStdout: true, script: 'git log --format=\"%H\" -n 1']).trim()}",
             "committer=${sh([returnStdout: true, script: 'git log --format=\"%cn\" -n 1']).trim()}",
@@ -22,7 +22,7 @@ def call(Map args = [:]) {
             "identifier=${sh([returnStdout: true, script: 'uuidgen']).trim()}",
     ]) {
         withEnv([
-                "image=${registry}/${service_name}-${env.BRANCH_NAME}:${version}",
+                "image=${registry}/${name_id}:${version}",
                 "DOCKER_PARAMS= --log-driver json-file --net=container:network-${identifier} -e JENKINS_URL -e GIT_BRANCH=${env.BRANCH_NAME} -e GIT_COMMIT=${revision} -e CHANGE_ID -e BRANCH_NAME -e BUILD_NUMBER -e BUILD_URL ",
         ]) {
            DockerUtilities.pushImage(context)
