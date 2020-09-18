e4ee4799741ff0f09502abbeda8366e775e81a9f
Jas Rowinski <jas.rowinski@unity3d.com>
2020-09-18 11:47:42 -0700

    lets see if we can just use the global one

--- vars/cloudBuildPipeline.groovy
+++ vars/cloudBuildPipeline.groovy
@@ -110,9 +110,7 @@ def call(body) {
                                 //sh("[ -z \"\$(docker images -a | grep \"${DOCKER_REG}/${SERVICE_NAME} 2>/dev/null)\" ] || PullCustomImages(gkeStrCredsID: 'sa-gcp-jenkins')")
                                 PullCustomImages(gkeStrCredsID: 'sa-gcp-jenkins')
                                 docker.image("gcr.io/unity-labs-createstudio-test/basetools:1.0.0").inside("-w /workspace -v \${PWD}:/workspace -it") {
-                                    withCredentials([string(credentialsId: 'sa-gcp-jenkins', variable: 'GC_KEY')]) {
-                                        sh("gcloud auth activate-service-account --key-file=${GC_KEY}")
-                                    }
+                                    sh("gcloud auth activate-service-account --key-file=${gcpBucketCredential}")
                                     manifestDateCheck = sh(returnStdout: true, script: "python3 /usr/local/bin/gcp_bucket_check.py")
                                     println(manifestDateCheck) 
                                     VERSION = getVersion()
