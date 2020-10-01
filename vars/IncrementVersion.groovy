7cd29ee5cf3fa5f5dd7493bcf0b15c533799e7ec
Jas Rowinski <jas.rowinski@unity3d.com>
2020-10-01 09:20:01 -0700

    change buildManfiest from docker to artifacts as root key

--- vars/IncrementVersion.groovy
+++ vars/IncrementVersion.groovy
@@ -23,12 +23,12 @@ def call(Map args = [:]) {
     def dateFormat = new SimpleDateFormat("yyyyMMddHHmm")
     def date = new Date()
 
-    String result = sh(script: "jq '.docker.${SERVICE_NAME}[].version' ${buildManifest}| tail -1", returnStdout: true).trim()
+    String result = sh(script: "jq '.artifacts.${SERVICE_NAME}[].version' ${buildManifest}| tail -1", returnStdout: true).trim()
     // 1.0.0-build.43 will be 1.0.0
     def LATEST_VERSION = result.split("-")[0]
     if ( LATEST_VERSION == "" ) {
         echo "${SERVICE_NAME} does not exist in our build manifest. Will begin with version 0.1.0-build.${BUILD_NUMBER}"
-        sh ("jq '.docker += { \"${SERVICE_NAME}\": [{\"version\": \"0.1.0-build.${BUILD_NUMBER}\", \"tags\":{\"UUID\": \"${BUILD_UUID}\", \"last_build_time\": \"${date}\"}}]}' ${buildManifest} | sponge ${buildManifest}")
+        sh ("jq '.artifacts += { \"${SERVICE_NAME}\": [{\"version\": \"0.1.0-build.${BUILD_NUMBER}\", \"tags\":{\"UUID\": \"${BUILD_UUID}\", \"last_build_time\": \"${date}\"}}]}' ${buildManifest} | sponge ${buildManifest}")
         LATEST_VERSION = "0.1.0-build.${BUILD_NUMBER}"
         return LATEST_VERSION
     } else {
@@ -49,7 +49,7 @@ def call(Map args = [:]) {
         } else {
             new_version = "${LATEST_VERSION}-build.${BUILD_NUMBER}" 
         }
-        sh ("jq '.docker.\"${SERVICE_NAME}\" += [{\"version\": \"${new_version}\", \"tags\": { \"UUID\": \"${BUILD_UUID}\", \"last_build_time\": \"${date}\"}}]' ${buildManifest} | sponge ${buildManifest}")
+        sh ("jq '.artifacts.\"${SERVICE_NAME}\" += [{\"version\": \"${new_version}\", \"tags\": { \"UUID\": \"${BUILD_UUID}\", \"last_build_time\": \"${date}\"}}]' ${buildManifest} | sponge ${buildManifest}")
         return new_version
     }
 }
