f719be5c880d1c8e8b7d34c83903c2bc0dd47190
Jas Rowinski <jas.rowinski@unity3d.com>
2020-10-14 20:01:26 -0700

    some debug lines on version

--- vars/cloudBuildPipeline.groovy
+++ vars/cloudBuildPipeline.groovy
@@ -117,9 +117,9 @@ def call(body) {
                                 //sh("[ -z \"\$(docker images -a | grep \"${DOCKER_REG}/${SERVICE_NAME} 2>/dev/null)\" ] || PullCustomImages(gkeStrCredsID: 'sa-gcp-jenkins')")
                                 docker.image("gcr.io/unity-labs-createstudio-test/basetools:1.0.0").inside("-w /workspace -v \${PWD}:/workspace -it") {
                                     manifestDateCheckPre = sh(returnStdout: true, script: "python3 /usr/local/bin/gcp_bucket_check.py | grep Updated")
-                                    //println(manifestDateCheckPre)
+                                    println(manifestDateCheckPre)
                                     VERSION = IncrementVersion()
-                                    //echo "Version is ${VERSION}"
+                                    echo "Version is ${VERSION}"
                                 }
                             }
                         }
