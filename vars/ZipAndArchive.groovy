bac716f95322db8387071699739826118781959e
Jas Rowinski <jas.rowinski@unity3d.com>
2020-10-22 14:32:19 -0700

    archive the zip

--- vars/ZipAndArchive.groovy
+++ vars/ZipAndArchive.groovy
@@ -9,7 +9,7 @@ def call(Map args = [:]) {
         echo "Zip up ${mergedArgs.project}"
         zip zipFile: "${mergedArgs.project}.zip", archive: false, dir: "${mergedArgs.project}"
         echo "Archive Artifact ${mergedArgs.project}"
-        archiveArtifacts allowEmptyArchive: false, artifacts: "${mergedArgs.project}", fingerprint: true, followSymlinks: false
+        archiveArtifacts allowEmptyArchive: false, artifacts: "${mergedArgs.project}.zip", fingerprint: true, followSymlinks: false
     }
 
 }
